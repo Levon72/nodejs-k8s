336687d8a7cb9a3571d9af6e81fc1431651e4d6c
Stas <levon7299@gmail.com>
2024-06-28 13:24:16 +0000

    Test 23 env

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const port = 3000;
 
 const app = express();
 app.get('/', (req, res) => {
-    res.send('A Simple Node.js Server is Up and Running.......test GITHUB_REF_NAME')
+    res.send('A Simple Node.js Server is Up and Running.......test {{ github.ref_name }}   GITHUB_REF_NAME')
 })
 app.listen(port, ()=>{
     console.log(`Server is listening on port ${port}`)
